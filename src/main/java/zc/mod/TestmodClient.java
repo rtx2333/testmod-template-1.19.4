d894074f9bb4b27bdd3b9adc98716fc486530fb7
BigDick <3570182103@qq.com>
2024-06-03 17:59:28 +0800

    test

---
+++ src/main/java/zc/mod/TestmodClient.java
@@ -0,0 +1,10 @@
+package zc.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
