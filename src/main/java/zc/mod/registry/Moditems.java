a9d6478011557138cf6735d6c5dc24fd1e2538d9
BigDick <3570182103@qq.com>
2024-06-04 12:12:21 +0800

    7

--- src/main/java/zc/mod/registry/Moditems.java
+++ src/main/java/zc/mod/registry/Moditems.java
@@ -1,6 +1,5 @@
 package zc.mod.registry;
 
-import jdk.incubator.vector.VectorOperators;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
